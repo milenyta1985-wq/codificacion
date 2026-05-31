f577375ce533b3c970124840252ad8cb97474c62
andrea milena <milenyta1985@gmail.com>
2026-05-30 23:23:58 -0500

    esta es la conexion

---
+++ DB/Conexion.php
@@ -0,0 +1,31 @@
+<?php
+
+class Conexion{
+    private $host = "localhost"; 
+    private $usuario = "root";
+    private $password = "";
+    private $db = "industrial";
+
+    public function conectar(){
+        try {
+            $conexion = new PDO("mysql:host=$this->host;dbname=$this->db", $this->usuario, $this->password);
+            // $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // return $conexion;
+            echo "Conexion exitosa";
+        } catch (PDOException $e) {
+            echo "Error de conexion" . $e->getMessage();
+            // return null;
+
+        }
+    }
+}
+
+// $con = new Conexion();
+//     $resultado = $con->conectar();
+//     echo $resultado;
+
+
+
+
+
+?>
